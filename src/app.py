1a997f55ea36ec52d3367a003f62f8678f166d37
Igor Assunção <31285950+IgorAssuncao@users.noreply.github.com>
2018-08-14 07:49:25 -0300

    Updated app.py
    Added status code for found domains and subdomains but still having issues on getting the status codes for not found subdomains.

--- src/app.py
+++ src/app.py
@@ -11,14 +11,15 @@ def getTargetInput():
 	return target
 
 def getWordlist():
-	wordlist = str(input('Path to Wordlist (you can leave blank if you want to use our default): '))
-	if wordlist == '':
-		wordlist = './wordlist/wordlist.txt'
-	return wordlist
+# 	wordlist = str(input('Path to Wordlist (you can leave blank if you want to use our default): '))
+# 	if wordlist == '':
+# 		wordlist = './wordlist/wordlist.txt'
+# 	return wordlist
+	return ["edu", "xxx"]
 
 def mountDomainUrl(target_url):
 	url_prefix = 'http://'
-	mountedUrl = '{}.{}' .format(url_prefix, target_url)
+	mountedUrl = '{}{}' .format(url_prefix, target_url)
 	return mountedUrl
 
 def checkDomain(target_domain_url):
@@ -26,7 +27,7 @@ def checkDomain(target_domain_url):
 		statusCodes = [200, 301, 302, 401, 403]
 		req = requests.get(target_domain_url)
 		if req.status_code in statusCodes:
-			print('Domain found: {}' .format(target_domain_url))
+			print('Domain found: {} - status_code: {}' .format(target_url, req.status_code))
 	except:
 		print('Domain "{}" not found' .format(target_domain_url))
 
@@ -36,24 +37,28 @@ def mountSubdomainUrl(target_url, wordlist_item):
 	return mountedUrl
 
 def makeRequest(target_url):
-	req = requests.get(target_url)
+	statusCodes = [200, 301, 302, 401, 403]
 	try:
-		if req.status_code == 200 or req.status_code == 301 or req.status_code == 302:
+		req = requests.get(target_url)
+		if req.status_code in statusCodes:
 			print('Subdomain "{}" found - status_code: {}' .format(target_url, req.status_code))
 	except:
-		print('Subdomain "{}" not found - status_code: {}' .format(target_url, req.status_code))
+		print('Subdomain "{}" not found' .format(target_url))
 	
 
 def main():
-	target = getTargetInput()
+	targetDomain = getTargetInput()
 	wordlist = getWordlist()
-	wordlistFile = open(wordlist, "r")
-	for wordlist_item in wordlistFile:
-		wordlist_item = wordlist_item.rstrip()
-		target_url = mountSubdomainUrl(target, wordlist_item)
-		makeRequest(target_url)
+	targetDomainUrl = mountDomainUrl(targetDomain)
+	checkDomain(targetDomainUrl)
+	if checkDomain(targetDomainUrl):
+		wordlistFile = open(wordlist, "r")
+		for wordlist_item in wordlistFile:
+			wordlist_item = wordlist_item.rstrip()
+			target_url = mountSubdomainUrl(target, wordlist_item)
+			makeRequest(target_url)
 
-	wordlistFile.close()
+		wordlistFile.close()
 		
 
 if __name__ == '__main__':
